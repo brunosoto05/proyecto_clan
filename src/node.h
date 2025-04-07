1820f5e0feb4d7123419d0a1e78229a6f3ed6b02
brunosoto05 <st.brunoalfonoso@gmail.com>
2025-04-06 22:51:29 -0400

    feat:archivo_nodo

---
+++ src/node.h
@@ -0,0 +1,27 @@
+#ifndef NODO_H
+#define NODO_H
+
+#include <string>
+
+struct Nodo {
+    int id;
+    std::string name;
+    std::string last_name;
+    char gender;  // 'H' o 'M'
+    int age;
+    int id_father;
+    bool is_dead;
+    bool was_chief;
+    bool is_chief;
+    
+    Nodo* padre;
+    Nodo* hijoIzquierdo;   // Primer hijo
+    Nodo* hermanoDerecho;   // Siguiente hermano
+    
+    // Lista de contribuidores (implementada como lista enlazada)
+    Contribuidor* contribuyentes;
+    
+    Nodo();
+};
+
+#endif
